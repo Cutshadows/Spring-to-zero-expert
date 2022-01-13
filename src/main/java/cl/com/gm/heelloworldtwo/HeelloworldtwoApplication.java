4cf29064a1fd46c625f39f2767dda981b7793168
Cutshadows <ramnstein_1749_@hotmail.com>
2022-01-13 12:48:57 -0300

    add ignore

---
+++ src/main/java/cl/com/gm/heelloworldtwo/HeelloworldtwoApplication.java
@@ -0,0 +1,15 @@
+package cl.com.gm.heelloworldtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeelloworldtwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeelloworldtwoApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/cl/com/gm/heelloworldtwo/HeelloworldtwoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.com.gm.heelloworldtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeelloworldtwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
